35fb454dc00b4f4b5ffb55669d905145b7bc7a73
masonjm <masonjm@gmail.com>
2014-06-06 20:18:48 -0700

    Set root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :imports, only: [:index, :create]
 
+  root 'imports#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
